6444df216080ad3088e31b3264cb9b2f34c9c570
voomp <bat7gamer7@gmail.com>
2020-11-16 16:34:05 -0500

    Flag Logo Support

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CountrySearch from './country/CountrySearch';
 import CountryData from './country/CountryData';
 import './styles/Application.css';
@@ -6,10 +6,15 @@ import logo from './img/logo.png';
 import StateData from './state/StateData';
 import StateSearch from './state/StateSearch';
 import { CurrentCountry } from './data/Store';
+import { getCountryData } from './data/APICalls';
+
+let lastCountry: any;
+let logoClass: any = "logo";
 
 const Application: React.FC = (): any => {
 	
 	const [country] = useContext(CurrentCountry);
+	const [countryLogo, setCountryLogo]: any = useState(logo);
 
 	const onFooterClick = () => {
 		window.open('https://github.com/voomp');
@@ -20,12 +25,18 @@ const Application: React.FC = (): any => {
 	let stateClass = "";
 	if(country === "USA") {
 		stateClass = "state";
+	} if(country !== lastCountry && country !== "") {
+		lastCountry = country;
+		logoClass = "logo countryLogo";
+		getCountryData(country).then((res: any) => {
+			setCountryLogo(res["countryInfo"]["flag"]);
+		});
 	}
 	
 	return (
 		<>
 			<div id="header">
-				<img src={logo} alt="Logo" className="logo" />
+				<img src={countryLogo} alt="Logo" className={logoClass} />
 				<span id="logoName"><span id="coronaTracker">corona<span id="tracker">Tracker</span></span></span>
 			</div>
 			<div id="info">
